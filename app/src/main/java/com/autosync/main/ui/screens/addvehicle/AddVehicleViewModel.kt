479e6468cd2e4a121dfdff802326b2a7182cbed4
Guzman0406 <aguzmanlopez20@gmail.com>
2025-11-21 02:38:14 -0600

    Crear la clase AppDatabase y AddVehicleViewModel con Hilt

---
+++ app/src/main/java/com/autosync/main/ui/screens/addvehicle/AddVehicleViewModel.kt
@@ -0,0 +1,50 @@
+package com.autosync.main.ui.screens.addvehicle
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.autosync.main.data.local.dao.VehicleDao
+import com.autosync.main.data.local.model.Vehicle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddVehicleViewModel @Inject constructor(
+    private val vehicleDao: VehicleDao
+) : ViewModel() {
+
+    val marca = MutableStateFlow("")
+    val modelo = MutableStateFlow("")
+    val year = MutableStateFlow("")
+    val licensePlate = MutableStateFlow("")
+
+    fun onMarcaChange(value: String) {
+        marca.value = value
+    }
+
+    fun onModeloChange(value: String) {
+        modelo.value = value
+    }
+
+    fun onYearChange(value: String) {
+        year.value = value
+    }
+
+    fun onLicensePlateChange(value: String) {
+        licensePlate.value = value
+    }
+
+    fun saveVehicle() {
+        viewModelScope.launch {
+            val vehicle = Vehicle(
+                make = marca.value,
+                model = modelo.value,
+                year = year.value.toIntOrNull() ?: 0,
+                licensePlate = licensePlate.value
+            )
+            vehicleDao.insertVehicle(vehicle)
+        }
+    }
+}
