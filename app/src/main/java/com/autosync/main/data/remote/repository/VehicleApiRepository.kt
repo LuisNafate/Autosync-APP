3102cf6bc94c7fcc983f234dbb6898e3924e5b2b
Guzman0406 <aguzmanlopez20@gmail.com>
2025-11-21 03:47:32 -0600

    Adaptar el ViewModel y el Repo a la nueva API

--- app/src/main/java/com/autosync/main/data/remote/repository/VehicleApiRepository.kt
+++ app/src/main/java/com/autosync/main/data/remote/repository/VehicleApiRepository.kt
@@ -2,18 +2,19 @@ package com.autosync.main.data.remote.repository
 
 import android.util.Log
 import com.autosync.main.data.remote.VehicleApiService
-import com.autosync.main.data.remote.dto.CarDto
+import com.autosync.main.data.remote.nhtsa.dto.ModelDto
 import javax.inject.Inject
 
 class VehicleApiRepository @Inject constructor(
     private val apiService: VehicleApiService
 ) {
 
-    suspend fun getCarsByMake(make: String): List<CarDto> {
+    suspend fun getModelsForMake(make: String): List<ModelDto> {
         return try {
-            apiService.getCarsByMake(make = make)
+            val response = apiService.getModelsForMake(make = make)
+            response.results
         } catch (e: Exception) {
-            Log.e("VehicleApiRepository", "Error fetching cars: ", e)
+            Log.e("VehicleApiRepository", "Error fetching models: ", e)
             emptyList()
         }
     }
